b6ad0d46ec1e86e6fce01ec3644ea85582ffadc2
michelleenoe <122521774+michelleenoe@users.noreply.github.com>
2025-04-29 13:20:42 +0200

    changed name

--- js/api.js
+++
@@ -1,8 +0,0 @@
-// export const handleError = (error) => {
-//     document.querySelector('#errorText').innerText = error;
-//     document.querySelector('#error').classList.remove('hidden');
-// };
-
-// export const header = new Headers({
-//     'X-Session-Token': sessionStorage.getItem('food_repo_user_token')
-// });
