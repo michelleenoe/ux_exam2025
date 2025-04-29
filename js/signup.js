574633e1300a4f70d8bc0ab5af6201c8b10cf4d6
michelleenoe <122521774+michelleenoe@users.noreply.github.com>
2025-04-29 13:21:16 +0200

    edited name to match api and added e.targets for the form

--- js/signup.js
+++ js/signup.js
@@ -1,38 +1,41 @@
 // import { USERS_BASE_URL } from './info.js';
 // import { handleError } from './api.js';
+import { USERS_BASE_URL } from './utils.js';
 
-// document.querySelector('#frmSignup').addEventListener('submit', (e) => {
-//     e.preventDefault();
+export function signupForm() {
+    document.querySelector('#frmSignup').addEventListener('submit', (e) => {
+    e.preventDefault();
 
-//     // Password validation
-//     const password = e.target.txtPassword.value.trim();
-//     const repeatPassword = e.target.txtRepeatPassword.value.trim();
+    const password = e.target.txtPassword.value.trim();
+    const repeatPassword = e.target.txtRepeatPassword.value.trim();
 
-//     if (password !== repeatPassword) {
-//         handleError('Passwords must match.');
-//         return false;
-//     }
+    if (password !== repeatPassword) {
+        handleError('Passwords must match.');
+        return false;
+    }
+    const first_name = e.target.txtFirstName.value.trim();
+    const last_name = e.target.txtLastName.value.trim();
+    const email = e.target.txtEmail.value.trim();
+    const birth_date = e.target.txtBirthDate.value.trim();
+    const phone_number = e.target.txtPhone.value.trim();
 
-//     // Form submission
-//     const firstName = e.target.txtFirstName.value.trim();
-//     const lastName = e.target.txtLastName.value.trim();
-//     const email = e.target.txtEmail.value.trim();
+    const params = new URLSearchParams();
+    params.append('email', email);
+    params.append('first_name', first_name);
+    params.append('last_name', last_name);
+    params.append('password', password);
+    params.append('birhday', birth_date);
+    params.append('phone', phone_number);
 
-//     const params = new URLSearchParams();
-//     params.append('email', email);
-//     params.append('first_name', firstName);
-//     params.append('last_name', lastName);
-//     params.append('password', password);
-
-//     fetch(`${USERS_BASE_URL}/users`,
-//         {
-//             method: 'POST',
-//             body: params
-//         }
-//     )
-//     .then(response => response.json())
-//     .then(data => {
-//         window.location.href = 'login.htm';
-//     })
-//     .catch(handleError);
-// });
+    fetch(`${USERS_BASE_URL}/users`,
+        {
+            method: 'POST',
+            body: params
+        }
+    )
+    .then(response => response.json())
+    .then(() => {
+        window.location.href = 'login.html';
+    })
+    });
+}
