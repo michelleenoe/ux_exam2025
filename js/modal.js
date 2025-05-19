c65bee813900040c85dcce591d9227e2eb3ec307
michelleenoe <122521774+michelleenoe@users.noreply.github.com>
2025-05-19 14:35:42 +0200

    listener med esc, aria- og skjuler

--- js/modal.js
+++ js/modal.js
@@ -1,7 +1,23 @@
 export const showLoanModal = () => {
-  document.querySelector("#loan_modal").classList.add("show");
+  const modal = document.querySelector("#loan_modal");
+  modal.classList.add("show");
+  modal.setAttribute("aria-hidden", "false");
+  document.querySelector("#modal_close_btn").focus();
+  document.addEventListener("keydown", handleEscape);
 };
 
-document.querySelector("#modal_close_btn").addEventListener("click", () => {
-  document.querySelector("#loan_modal").classList.remove("show");
-});
+export const hideLoanModal = () => {
+  const modal = document.querySelector("#loan_modal");
+  modal.classList.remove("show");
+  modal.setAttribute("aria-hidden", "true");
+  document.removeEventListener("keydown", handleEscape);
+  document.querySelector(".loan_btn").focus();
+};
+
+function handleEscape(e) {
+  if (e.key === "Escape" || e.key === "Esc") {
+    hideLoanModal();
+  }
+}
+document.querySelector("#modal_close_btn")
+        .addEventListener("click", hideLoanModal);
