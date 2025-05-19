82ed09352862bf120ac369e93317a33f5ecbd343
michelleenoe <122521774+michelleenoe@users.noreply.github.com>
2025-05-19 13:05:21 +0200

    webp

--- js/info.js
+++ js/info.js
@@ -1,2 +1,2 @@
 export const BASE_URL = "http://localhost:8080";
-export const FALLBACK_IMAGE = "../assets/images/placeholder-cover.jpg";
+export const FALLBACK_IMAGE = "../assets/images/placeholder-cover.webp";
